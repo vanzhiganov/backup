f4cdd83c70d44813c2855871cc43a8ad61a05773
vanzhiganov <vanzhiganov@ya.ru>
2015-07-19 18:59:08 +0300

    update

--- backup.py
+++ backup.py
@@ -14,6 +14,29 @@ import gnupg
 import easywebdav
 from ftplib import FTP
 
+
+def BackupMysql(host, user, password, database, destination):
+    cmd = '/usr/bin/mysqldump -h %(host)s -u%(user)s --password=%(password)s %(database)s > %(destination)s' % {
+        "host": host,
+        "user": user,
+        "password": password,
+        "database": database,
+        "destination": destination
+    }
+    os.system(cmd)
+
+
+def BackupPgsql(host, user, password, database, destination):
+    cmd = """PGPASSWORD="%(password)s" pg_dump -h %(host)s -U%(user)s %(database)s > %(destination)s""" % {
+        "host": host,
+        "user": user,
+        "password": password,
+        "database": database,
+        "destination": destination,
+    }
+    os.system(cmd)
+
+
 parser = OptionParser()
 parser.add_option("-c", "--config", dest="config_filename", default="config.ini", help="read from FILE", metavar="CONFIG")
 parser.add_option("-l", "--log", dest="log_filename", default="backup.log", help="write report to FILE", metavar="FILE")
@@ -102,13 +125,13 @@ for x in config:
     if not "Enabled" in config[x]:
         continue
 
-    (datatype, jobname) = x.split(":")
+    (datatype, job_name) = x.split(":")
 
     if datatype == "File":
         if config[x]['Enabled'] == "no":
             continue
 
-        logging.info("Backup files. Job: %s" % jobname)
+        logging.info("Backup files. Job: %s" % job_name)
 
 
         date_format = ["%A %d.%m.%Y", "%Y%m%d"]
@@ -117,7 +140,7 @@ for x in config:
         b_date = datetime.date.today().strftime(date_format[default_date_format])
 
         b_storagelocal = config['DEFAULT']['StorageLocal']
-        b_archivename = "%s_%s_%s.tar" % (config['DEFAULT']['InstanceName'], jobname, b_date)
+        b_archivename = "%s_%s_%s.tar" % (config['DEFAULT']['InstanceName'], job_name, b_date)
         b_source = config[x]['Directory']
         b_destination = "%s/%s" % (b_storagelocal, b_archivename)
 
@@ -199,86 +222,93 @@ for x in config:
         if config[x]['Enabled'] == "no":
             continue
 
-        print "Backup database. Job: %s" % jobname
+        print "Backup database. Job: %s" % job_name
 
         date_format = ["%A %d.%m.%Y", "%Y%m%d"]
         default_date_format = 1
 
-        b_storagelocal = config['DEFAULT']['StorageLocal']
-        b_date = datetime.date.today().strftime(date_format[default_date_format])
-        b_archivename = "%s_%s_%s_%s.sql" % (config['DEFAULT']['InstanceName'], config[x]['Engine'], config[x]['Database'], b_date)
-        b_destination = "%s/%s" % (b_storagelocal, b_archivename)
+        # example: /var/backups
+        storage_local_path = config['DEFAULT']['StorageLocal']
 
+        current_date = datetime.date.today().strftime(date_format[default_date_format])
+
+        # example: /var/backups/instance/job/20100101
+        archive_path = "%s/%s/%s/%s" % (storage_local_path, config['DEFAULT']['InstanceName'], job_name, current_date)
+        # example: mysql_mydatabase.sql
+        archive_name = "%s_%s.sql" % (config[x]['Engine'], config[x]['Database'])
+        # example: /var/backups/instance/job/20100101/mysql_mydatabase.sql
+        archive_local_destination = archive_path + '/' + archive_name
+
+        # make backup
         if config[x]['Engine'] == "mysql":
-            mc = {
-                "host": config[x]['Host'],
-                "user": config[x]['User'],
-                "password": config[x]['Password'],
-                "database": config[x]['Database'],
-                "destination": b_destination
-            }
-            cmd = '/usr/bin/mysqldump -h %(host)s -u%(user)s --password=%(password)s %(database)s > %(destination)s' % mc
-            os.system(cmd)
-        elif config[x]['Engine'] == "pgsql":
-            pc = {
-                "user": config[x]['User'],
-                "password": config[x]['Password'],
-                "host": config[x]['Host'],
-                "database": config[x]['Database'],
-                "destination": b_destination,
-            }
+            # create folder
+            subprocess.call(['mkdir', '-p', archive_path])
 
-            cmd = """PGPASSWORD="%(password)s" pg_dump -h %(host)s -U%(user)s %(database)s > %(destination)s""" % pc
-            os.system(cmd)
+            # BackupMysql('localhost', 'dbuser', 'dbpasswd', 'mydatabase', '/var/backups/pgsql_20100101.sql')
+            BackupMysql(config[x]['Host'], config[x]['User'], config[x]['Password'], config[x]['Database'], archive_local_destination)
+        elif config[x]['Engine'] == "pgsql":
+            # create folder
+            subprocess.call(['mkdir', '-p', archive_path])
+
+            # BackupPgsql('localhost', 'dbuser', 'dbpasswd', 'mydatabase', '/var/backups/pgsql_20100101.sql')
+            BackupPgsql(config[x]['Host'], config[x]['User'], config[x]['Password'], config[x]['Database'], archive_local_destination)
         else:
             continue
 
+        # check for compression
         if config[x]['Compression'] == "yes":
+            # TODO: add compression level
             subprocess.call(['gzip', b_destination])
-            # archive filename
-            b_archivename = "%s.gz" % b_archivename
-            # archive filename with path
-            b_destination = "%s.gz" % b_destination
-            
+            # example: mysql_mydatabase.sql.gz
+            archive_name = "%s.gz" % archive_name
+            # example: /var/backups/instance/20100101/mysql_mydatabase.sql.gz
+            archive_local_destination = archive_path + '/' + archive_name
+
+        # check for upload to remote resource
         if "Remote" in config[x]:
             remote = {}
             remote['name'] = 'REMOTE:%s' % config[x]['Remote']
 
             if remote['name'] in config:
                 remote['type'] = config[remote['name']]['Type']
-                
+
                 if remote['type'] == "webdav":
                     remote['host'] = config[remote['name']]['Host']
                     remote['port'] = int(config[remote['name']]['Port'])
                     remote['protocol'] = config[remote['name']]['Protocol']
                     remote['login'] = config[remote['name']]['Login']
                     remote['password'] = config[remote['name']]['Password']
-                    remote['destination'] = "%s/%s/%s" % (config['DEFAULT']['InstanceName'], jobname, b_archivename)
+                    # example: instance/job/20100101/pgsql_20100101.sql.gz
+                    remote['destination'] = "%s/%s/%s/%s" % (config['DEFAULT']['InstanceName'], job_name, current_date, archive_name)
 
                     # connect to webdav
                     webdav = easywebdav.connect(remote['host'], port=remote['port'], protocol=remote['protocol'], username=remote['login'], password=remote['password'])
-                    # todo: check exists folder
+
+                    # check exists folders
                     if not webdav.exists(config['DEFAULT']['InstanceName']):
                         webdav.mkdir(config['DEFAULT']['InstanceName'])
 
-                    if not webdav.exists(config['DEFAULT']['InstanceName'] + "/" + jobname):
-                        webdav.mkdir(config['DEFAULT']['InstanceName']+"/"+jobname)
+                    if not webdav.exists(config['DEFAULT']['InstanceName'] + "/" + job_name):
+                        webdav.mkdir(config['DEFAULT']['InstanceName'] + "/" + job_name)
+
+                    if not webdav.exists(config['DEFAULT']['InstanceName'] + "/" + job_name + '/' + current_date):
+                        webdav.mkdir(config['DEFAULT']['InstanceName'] + "/" + job_name + '/' + current_date)
 
                     # upload archive to webdav
-                    webdav.upload(b_destination, remote['destination'])
+                    webdav.upload(archive_local_destination, remote['destination'])
                 elif remote['type'] == "ftp":
                     remote['host'] = config[remote['name']]['Host']
                     remote['port'] = int(config[remote['name']]['Port'])
                     #remote['protocol'] = config[remote['name']]['Protocol']
                     remote['login'] = config[remote['name']]['Login']
                     remote['password'] = config[remote['name']]['Password']
-                    remote['destination'] = "%s/%s/%s" % (config['DEFAULT']['InstanceName'], jobname, b_archivename)
+                    remote['destination'] = "%s/%s/%s/%s" % (config['DEFAULT']['InstanceName'], job_name, current_date, archive_name)
 
                     ftp = FTP(remote['host'])
                     ftp.login(remote['login'], remote['password'])
 
                     # todo: check exists folder
-                    #if not ftp.exists(config['DEFAULT']['InstanceName']):
+                    # if not ftp.exists(config['DEFAULT']['InstanceName']):
                     try:
                         ftp.mkd(config['DEFAULT']['InstanceName'])
                     except:# Exception as e:
@@ -286,17 +316,27 @@ for x in config:
 
                     #if not ftp.exists(config['DEFAULT']['InstanceName'] + "/" + jobname):
                     try:
-                        ftp.mkd(config['DEFAULT']['InstanceName']+"/"+jobname)
+                        ftp.mkd(config['DEFAULT']['InstanceName'] + "/" + job_name)
                     except:# Exception as e:
-                        print "oops 2"
+                        print "oops 3"
 
-                    ftp.cwd(config['DEFAULT']['InstanceName']+"/"+jobname)
+                    #if not ftp.exists(config['DEFAULT']['InstanceName'] + "/" + jobname):
+                    try:
+                        ftp.mkd(config['DEFAULT']['InstanceName'] + "/" + job_name + "/" + current_date)
+                    except:# Exception as e:
+                        print "oops 4"
 
-                    with open(b_destination, 'rb') as ftpup:
-                        ftp.storbinary('STOR ' + b_archivename, ftpup)
+                    ftp.cwd(config['DEFAULT']['InstanceName'] + "/" + job_name + "/" + current_date)
+
+                    with open(archive_local_destination, 'rb') as ftpup:
+                        ftp.storbinary('STOR ' + archive_name, ftpup)
                         ftp.close()
 
-#
+        # check for remove
+        if 'SaveLocal' in config[x]:
+            if config[x]['SaveLocal'] == "no" or config[x]['SaveLocal'] == 0:
+                subprocess.call(['rm', archive_local_destination])
+
 #
 # # if __name__ == '__main__':
 #     # backup.backup().main()
