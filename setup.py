54fc3c913ff615ba7c0f6171462caafc1f4aa687
vanzhiganov <vanzhiganov@ya.ru>
2016-03-02 14:07:12 +0300

    start setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='backup',
+    version='1.1.7',
+    author='Vyacheslav Anzhiganov',
+    author_email='hello@anzhiganov.com',
+    package_data={},
+    scripts=[
+        'backup.py',
+    ],
+    packages=[
+    ],
+    install_requires=[
+    ]
+)
